456a0469f69b26562f090c11afa675d2bbe968b3
Arttu Multanen <karethoth@gmail.com>
2014-01-05 19:28:05 +0200

    Wrote MeshManager class.
    It doesn't support loading resources by name. At least yet.

---
+++ src/meshManager.cc
@@ -0,0 +1,13 @@
+#include "meshManager.hh"
+
+
+MeshManager::~MeshManager()
+{
+}
+
+bool MeshManager::Load( std::string &meshFile )
+{
+	// yup, doesn't do a thing.
+	return true;
+}
+
---
+++ src/meshManager.hh
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _MESHMANAGER_HH_
+#define _MESHMANAGER_HH_
+
+#include "templateManager.hh"
+#include "mesh.hh"
+
+
+class MeshManager : public TemplateManager<Mesh>
+{
+ public:
+	virtual ~MeshManager();
+
+	// Doesn't do a thing.
+	virtual bool Load( std::string &meshFile );
+};
+
+
+#endif
+
