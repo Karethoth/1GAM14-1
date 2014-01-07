436572267df0284109930d3d139f3d22290a48b2
Arttu Multanen <karethoth@gmail.com>
2014-01-07 16:39:24 +0200

    Added few helper functions.

--- src/helpers.cc
+++ src/helpers.cc
@@ -14,3 +14,43 @@ double ToRadians( double degrees )
 	return  degrees * piDivided;
 }
 
+
+
+float Cross( const glm::vec2& a, const glm::vec2& b )
+{
+	glm::vec3 v1( a.x, a.y, 0.0f );
+	glm::vec3 v2( b.x, b.y, 0.0f );
+
+	return ( v1, v2 ).z;
+}
+
+
+std::shared_ptr<glm::vec2> PointInBetween( const glm::vec2& a,
+                                           const glm::vec2& b,
+                                           double ratio )
+{
+	if( ratio > 1.0 )
+		ratio = 1.0;
+	else if( ratio < 0.0 )
+		ratio = 0.0;
+
+	glm::vec2 delta = b-a;
+	delta *= ratio;
+
+	return std::make_shared<glm::vec2>( delta + a );
+}
+
+
+
+std::shared_ptr<glm::vec2> Intersection( const glm::vec2& a,
+                                         const glm::vec2& bOffset,
+                                         const glm::vec2& c,
+                                         const glm::vec2& dOffset )
+{
+	float t = Cross( (c-a), dOffset / ( Cross( bOffset, dOffset ) ) );
+	float u = Cross( (c-a), bOffset / ( Cross( bOffset, dOffset ) ) );
+
+	auto point = std::make_shared<glm::vec2>();
+	return point;
+}
+
--- src/helpers.hh
+++ src/helpers.hh
@@ -2,9 +2,24 @@
 #ifndef _HELPERS_HH_
 #define _HELPERS_HH_
 
+#include "../deb/glm/glm.hpp"
+#include <memory>
+
 
 float  ToRadians( float degrees );
 double ToRadians( double degrees );
+
+
+float Cross( const glm::vec2& a, const glm::vec2& b );
+
+std::shared_ptr<glm::vec2> PointInBetween( const glm::vec2& a,
+                                           const glm::vec2& b,
+                                           double ratio );
+
+std::shared_ptr<glm::vec2> Intersection( const glm::vec2& a,
+                                         const glm::vec2& bOffset,
+                                         const glm::vec2& c,
+                                         const glm::vec2& dOffset );
 
 #endif
 
